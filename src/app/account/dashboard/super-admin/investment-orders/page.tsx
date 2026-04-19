6e35637e252a5ebde507b7d62eb026bc63ea421a
superAdminMail <superAdminMail@proton.me>
2026-04-20 00:34:39 +0100

    fixed prerender error

--- src/app/account/dashboard/super-admin/investment-orders/page.tsx
+++ src/app/account/dashboard/super-admin/investment-orders/page.tsx
@@ -1,6 +1,8 @@
 import { AdminInvestmentOrdersClient } from "@/app/account/dashboard/admin/investment-orders/_components/AdminInvestmentOrdersClient";
 import { getAdminInvestmentOrders } from "@/actions/admin/investment-order/getAdminInvestmentOrders";
 
+export const dynamic = "force-dynamic";
+
 export default async function SuperAdminInvestmentOrdersPage() {
   const data = await getAdminInvestmentOrders();
 
